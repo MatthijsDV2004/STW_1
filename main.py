fca995fa6c0e32e570e08898ba870671bb6d4764
MatthijsDV2004 <mdevries@csumb.edu>
2024-04-05 18:34:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
-from datetime import datetime
-import requests, json
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
